ca49a14a41713b731d4e5aed2553557938eed75e
unknown <danieleidenilsen@hotmail.com>
2020-02-11 10:15:05 +0100

    update

--- js/game.js
+++ js/game.js
@@ -9,12 +9,12 @@ for(var i = 13; i>0;i--)  {
     bord.innerHTML += "<div class = 'brick'>"+[i]+"</div>";
 
 }
-for(var c = 14;c < 30; c++) { 
+for(var c = 14;c <= 28; c++) { 
 
     bord2.innerHTML += "<div class = 'brick briks2'>"+[c]+"</div>";
 
 }
-for(var c = 14;c < 30; c++) { 
+for(var c = 30;c <= 40; c++) { 
 
     board3.innerHTML += "<div class = 'brick brick3'>"+[c]+"</div>";
 
