24ad46854e6addab58e43f73832c8c669c1cfef9
unknown <danieleidenilsen@hotmail.com>
2020-02-11 14:27:33 +0100

    update

--- js/game.js
+++ js/game.js
@@ -4,19 +4,22 @@
 var bord = document.getElementById('bordbriks');
 var bord2 = document.getElementById('bordbrikstwo');
 var board3 = document.getElementById('bordbriks3');
+var boardfour = document.getElementById('boardfour');
 
-for(var i = 13; i>0;i--)  { 
+for(var i = 14; i>0;i--)  { 
     bord.innerHTML += "<div class = 'brick'>"+[i]+"</div>";
-
 }
-for(var c = 14;c <= 28; c++) { 
-
+for(var c = 11;c < 26; c++) { 
     bord2.innerHTML += "<div class = 'brick briks2'>"+[c]+"</div>";
-
 }
-for(var c = 30;c <= 40; c++) { 
-
+for(var c = 26;c < 37; c++) { 
     board3.innerHTML += "<div class = 'brick brick3'>"+[c]+"</div>";
-
 }
+
+for(var c = 38;c <45; c++) { 
+    boardfour.innerHTML += "<div class = 'brick'>"+[c]+"</div>";
+}
+
+
+
 
