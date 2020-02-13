044590ba27b23757e426937ab70e1db124f32627
unknown <danieleidenilsen@hotmail.com>
2020-02-13 14:03:51 +0100

    update

--- js/charercter.js
+++ js/charercter.js
@@ -1,48 +1,53 @@
-
+var my_array = []
 var charectArray = [583,55,30,40,50,88,66,350,150,23];
+
 for (var i = 0; i < charectArray.length; i++) { 
     fetch("https://anapioficeandfire.com/api/characters/"+charectArray[i]).then(result => result.json()).then((rest) => { 
-        resu(rest);    
-    
+        resu(rest);   
+      
+        my_array.push(rest.name);
+        
+        const cards = document.querySelectorAll('.cardOne');
+        
+        function GetCards() { 
+            console.log(this);
+        }
+        cards.forEach(card => card.addEventListener('click',GetCards));
+            
+       
         
       
     }).catch(error => console.log(error))
 
 }        
-    function resu(result) {       
-
-
-        document.getElementById('id').innerHTML += "<div  id = "+ result.name +" class = 'col-11 col-sm-11 col-md-11 col-lg-5 cardOne'><h1>" + result.name + "</h1> <h4> " + result.gender+ "</h4> <h4> "+ result.titles + "</h4></div>";
-        displayCard();  
-       var name =  document.getElementById(result.name)
-       name.addEventListener('click',function() { 
-           console.log("Hello w orl")
-       })
-      
-            
-        
-  
+    function resu(result) {            
+        document.getElementById('id').innerHTML += "<div class = 'col-11 col-sm-11 col-md-11 col-lg-5 cardOne'><h1>" + result.name + "</h1> <h4> " + result.gender+ "</h4> <h4> "+ result.titles + "</h4></div>";
+        displayCard(); 
+        my_array.push(result.name);      
+   
     }
-
     
-
+   
 
 
 function delay(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
 }
 
+
 async function displayCard ()  {  
     var getcards = document.querySelectorAll(".cardOne")
  
     for(var i = 0; i< getcards.length;i++)  {
-      
-        await delay(300);
-        if(getcards[i].style.display === "block") {
+              await delay(300);
+        if(getcards[i].style.display === "block") {            
             getcards[i].style.display = "none";
         } else { 
             getcards[i].style.display = "block";
         }    
     }  
 };
+
+
+
 
