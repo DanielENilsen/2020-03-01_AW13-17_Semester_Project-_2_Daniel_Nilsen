fed423422a5a4509dcf174a35487ab164e584433
unknown <danieleidenilsen@hotmail.com>
2020-02-18 13:58:04 +0100

    update

--- js/game.js
+++ js/game.js
@@ -12,37 +12,39 @@ var traps = [8,13,30,21,28,35];
 
 var stand = 1;
 var stand2 = 1;
-var dice = document.getElementById('dice');
+var dice = document.getElementById('dice')
+dice.innerHTML = "rolldice"
 
 
 
 
 for(var i = 13; i>0;i--)  { 
 
-    bord3.innerHTML += "<div  id = "+[i]+" class = 'brick'>"+[i]+"</div>";
+    bord3.innerHTML += "<div  id = "+[i]+" class = 'brick'></div>";
 
 }
-for(var c = 14;c <= 29; c++) { 
+for(var c = 14;c <= 28; c++) { 
 
-    bord2.innerHTML += "<div id = "+[c]+" class = 'brick briks2'>"+[c]+"</div>";
+    bord2.innerHTML += "<div id = "+[c]+" class = 'brick briks2'></div>";
 
 }
 for(var c = 29;c <= 40; c++) { 
 
-    bord.innerHTML += "<div id = "+[c]+" class = 'brick brick3'>"+[c]+"</div>";
+    bord.innerHTML += "<div id = "+[c]+" class = 'brick brick3'></div>";
 }
 traps.forEach(element => {
     document.getElementById(element).style.background = "red"
     
 });
-    
+
 
 dice.addEventListener("click",function() { 
     var randomNumber = Math.floor(Math.random()*6)+1;
     var randomNumber2 = Math.floor(Math.random()*6)+1;
+    dice.innerHTML=randomNumber
     
-    document.getElementById(stand).innerHTML = stand;
-    document.getElementById(stand2).innerHTML = stand2;
+    document.getElementById(stand).innerHTML = "";
+    document.getElementById(stand2).innerHTML = "";
 
 
     stand+=randomNumber;
@@ -52,41 +54,24 @@ dice.addEventListener("click",function() {
     console.log(`Player two are on squere ${stand2}`)
     console.log("")
 
-    if(stand < 40) { 
-       
-        document.getElementById(stand).innerHTML = "<img src='../playerIcon/iconOne.png'></img>";
-        
+    if(stand < 40) {        
+        document.getElementById(stand).innerHTML = "<img  src='../playerIcon/iconOne.png'></img>";        
       
     } else {
         alert("Game over Player One ")
     }
 
     if (stand2 < 40) { 
-        document.getElementById(stand2).innerHTML = null;
+      
     document.getElementById(stand2).innerHTML = "<img src='../playerIcon/player_icon_two.png'></img>";
    } else {
     alert("Game over player TWo won")
+    }
 
 
-
-
-}
-   
+  
     
 });
-
-
-
- 
-
-   
-  
-
-
-
-
-
-
 
 
 
