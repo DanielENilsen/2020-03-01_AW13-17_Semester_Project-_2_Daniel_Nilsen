84fd40bbff32fb120aa5fb8ddb5c741f12b7f908
unknown <danieleidenilsen@hotmail.com>
2020-02-24 11:34:51 +0100

    update

--- js/charercter.js
+++ js/charercter.js
@@ -24,7 +24,7 @@ for (var i = 0; i < charectArray.length; i++) {
     }).catch(error => console.log(error));
 }        
 function resu(result) {            
-    document.getElementById('id').innerHTML += "<div id ='jee' class = 'col-11 col-sm-11 col-md-11 col-lg-4 cardOne cardupdate'><h1>" + result.name + "</h1></div>";
+    document.getElementById('id').innerHTML += "<div id ='jee' class = 'col-11 col-sm-11 col-md-5 col-lg-5 col-xl-4 cardOne cardupdate'><h1>" + result.name + "</h1></div>";
     displayCard();       
 }     
 
