e9825059213b873c1cb91f07b57aa7d90debd5d6
unknown <danieleidenilsen@hotmail.com>
2020-02-24 18:51:19 +0100

    update

--- js/game.js
+++ js/game.js
@@ -1,10 +1,19 @@
 
+class player { 
+    constructor(name,img) { 
+        this.name = name;
+        this.img = img;       
+    }
+}
+
+
+
 var bord = document.getElementById('bordOne');
 var bord2 = document.getElementById('bordbrikstwo');
 var bord3 = document.getElementById('boardthree');
 var traps = [8,13,30,21,28,35];
-var stand = 1;
-var stand2 = 1;
+var stand =  new player("PlayerOne", "<img  src='../img/Untitled-1.png'>");
+var stand2 = new player("Playertwo", "<img  src='../img/Untitled-1.png'>");;
 var dice = document.getElementById('dice');
 dice.innerHTML = "rolldice";
 for(var i = 13; i>0;i--)  { 
@@ -15,6 +24,7 @@ for(var c = 14;c <= 28; c++) {
 }
 for(var c = 29;c <= 40; c++) { 
     bord.innerHTML += "<div id = "+[c]+" class = 'brick brick3'></div>";
+    
 }
 traps.forEach(element => {
     document.getElementById(element).style.background = "red";  
@@ -29,28 +39,33 @@ dice.addEventListener("click",function() {
     
     var filterOne = traps.filter(function(number) { 
         if (number == stand) {
-            return alert(`Player one Trap on ${number}`);
+            ///return alert(`Player one Trap on ${number}`);
         }     
-      
-    });
-    var filterOnetwo = traps.filter(function(number) { 
-        if (number == stand2) {
-            return alert(`Player two Trap on ${number}`);
-        }     
-      
-    });
 
-
-    dice.innerHTML=randomNumber;
+        traps.forEach(element => {
+            document.getElementById(element).style.background = "red"; 
+            if(filterOne == element) { 
+                console.log(stand);
+            }      
+        });    
+    });
+  
     
-    document.getElementById(stand).innerHTML = "";
-    document.getElementById(stand2).innerHTML = "";
+    dice.innerHTML=randomNumber;    
+    //document.getElementById(stand).innerHTML = "";
+    //document.getElementById(stand2).innerHTML = "";
 
     stand+=randomNumber;
-    stand2+=randomNumber2;
+    stand2+=randomNumber2;     
+    
+  
+    if(stand == 8 || stand2 == 8) { 
+        this.name;     
+        
+    }
 
-   
-
+    console.log(`Plater one are on ${stand}`)
+    console.log(`Plater one are on ${stand2}`)
     if(stand < 40) {        
         document.getElementById(stand).innerHTML = "<img  src='../img/Untitled-1.png'></img>";        
       
