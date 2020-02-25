c3293102612775b9189ba8b498aefbeb735d1aca
unknown <danieleidenilsen@hotmail.com>
2020-02-25 14:30:42 +0100

    update

--- js/game.js
+++ js/game.js
@@ -15,6 +15,7 @@ var stand =  new player("PlayerOne",1,"<img src='../img/Untitled-1.png'>");
 var stand2 = new player("Playertwo",1,"<img src='../img/Untitled-1.png'>");
 var dice = document.getElementById('dice');
 dice.innerHTML = "rolldice";
+let PlayDice = true;
 
 for(var i = 13; i>0;i--)  { 
     bord3.innerHTML += "<div  id = "+[i]+" class = 'brick'></div>";
@@ -30,45 +31,38 @@ traps.forEach(element => {
 
 });
 
-dice.addEventListener("click",function() { 
-    var randomNumber = Math.floor(Math.random()*6)+1;
-    var randomNumber2 = Math.floor(Math.random()*6)+1;
-    
-    var filterOne = traps.filter(function(number) { 
-        if (number == stand) {
-            ///return alert(`Player one Trap on ${number}`);
-        }     
-
-        traps.forEach(element => {
-            document.getElementById(element).style.background = "red"; 
-            if(filterOne == element) { 
-                console.log(stand);
-            }      
-        });    
-    });  
+function game () { 
+    let PlayDice = true;
+    let randomNumber = Math.floor(Math.random()*6)+1;
+    let randomNumber2 = Math.floor(Math.random()*6)+1;
     
     dice.innerHTML=randomNumber;    
-   document.getElementById(stand.brick).innerHTML = "";
-   document.getElementById(stand2.brick).innerHTML = "";
-
+    document.getElementById(stand.brick).innerHTML = "";
+    document.getElementById(stand2.brick).innerHTML = "";
     stand.brick+=randomNumber;
-    stand2.brick+=randomNumber2;     
-    if(stand.brick == 8) { 
-        dice.removeEventListener('mousedown');
-        
-        document.getElementById('gameTasks').innerHTML = "Hello"
-
-                        
-    }
-    if(stand2.brick == 8) { 
-        dice.removeEventListener('mousedown');
+    stand2.brick+=randomNumber2;    
        
-        document.getElementById('gameTasks').innerHTML = "Hell 2o"
-    }   
+
+    if(PlayDice == false) { 
+        dice.removeEventListener("click",playerMove);       
     
+        
+    
+    }
   
-    console.log(`Player one are on ${stand.brick}`)
-    console.log(`Player two are on ${stand2.brick}`)
+
+  function playerMove() {
+      if(stand.brick == 8) {
+        var local = false;
+        window.local = local;                    
+    }
+
+    if(stand2.brick == 8) { 
+        PlayDice = false;
+         
+    }  
+    
+    
     if(stand.brick < 40) {        
         document.getElementById(stand.brick).innerHTML = stand.img;    
               
@@ -80,9 +74,9 @@ dice.addEventListener("click",function() {
     } else {
          document.getElementById('bord').style.display = "none";
     } 
-    
-});
+}
+   
 
 
-
+}
 
