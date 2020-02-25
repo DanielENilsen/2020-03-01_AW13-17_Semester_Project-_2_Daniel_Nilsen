b7a575a25f26b8295b82f9d99babcfe050ed6a52
unknown <danieleidenilsen@hotmail.com>
2020-02-25 19:13:56 +0100

    update

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ class player {
         this.img = img;    
     }
 }
+let btn = document.getElementById('btn');
 
 var bord = document.getElementById('bordOne');
 var bord2 = document.getElementById('bordbrikstwo');
@@ -13,7 +14,8 @@ var bord3 = document.getElementById('boardthree');
 var traps = [8,13,30,21,28,35];
 var stand =  new player("PlayerOne",1,"<img src='../img/Untitled-1.png'>");
 var stand2 = new player("Playertwo",1,"<img src='../img/Untitled-1.png'>");
-var dice = document.getElementById('dice');
+let task_one = document.getElementById('taskone');
+
 dice.innerHTML = "rolldice";
 let PlayDice = true;
 
@@ -30,53 +32,90 @@ traps.forEach(element => {
     document.getElementById(element).style.background = "red";  
 
 });
-
+/*
 function game () { 
+    var dice = document.getElementById('dice');
     let PlayDice = true;
     let randomNumber = Math.floor(Math.random()*6)+1;
     let randomNumber2 = Math.floor(Math.random()*6)+1;
     
-    dice.innerHTML=randomNumber;    
+        
     document.getElementById(stand.brick).innerHTML = "";
     document.getElementById(stand2.brick).innerHTML = "";
     stand.brick+=randomNumber;
-    stand2.brick+=randomNumber2;    
+    stand2.brick+=randomNumber2;   
+    
+    function move(agr,arg2) { 
+        if(agr < 40) {        
+            document.getElementById(agr).innerHTML = stand.img;    
+                  
+        } else {
+            document.getElementById('bord').style.display = "none";
+        }
+        if (arg2 < 40) {       
+             document.getElementById(agr).innerHTML = stand2.img;
+        } else {
+             document.getElementById('bord').style.display = "none";
+        } 
+
+
+    }
        
 
-    if(PlayDice == false) { 
-        dice.removeEventListener("click",playerMove);       
-    
+    if(PlayDice == true) { 
+       //dice.removeEventListener("click",playerMove(stand.brick,stand2.brick));  
+       dice.addEventListener('click',move(stand.brick,stand2.brick))    
         
-    
-    }
-  
 
-  function playerMove() {
-      if(stand.brick == 8) {
-        var local = false;
-        window.local = local;                    
     }
 
-    if(stand2.brick == 8) { 
-        PlayDice = false;
-         
+}
+
+game ();
+   
+*/
+
+function Game() { 
+    var taskbtn = document.getElementById('gameTasks');
+    let task_1  = document.getElementById('hello');
+
+    var dice = document.getElementById ('dice');   
+
+    console.log("Hleo");
+    let play = true;
+
+
+    dice.addEventListener('click',function(){ 
+
+        document.getElementById(stand.brick).innerHTML = "";  
+        var randNumb = Math.floor(Math.random()*6)+1;        
+        stand.brick +=randNumb;
+        console.log(stand.brick);
+        moveplayer(stand.brick,stand.img)
+    });
+
+    function moveplayer(arg1,arg2) {   
+        if(arg1 == 8)   {
+            
+            dice.style.display = "none";
+            //task_1.style.display = "block";
+            document.getElementById('btn').style.display = "block";           
+              
+           
+        }        
+       
+        if(arg1 < 40) { 
+            return document.getElementById(arg1).innerHTML = arg2;
+        }      
     }  
     
     
-    if(stand.brick < 40) {        
-        document.getElementById(stand.brick).innerHTML = stand.img;    
-              
-    } else {
-        document.getElementById('bord').style.display = "none";
-    }
-    if (stand2.brick < 40) {       
-         document.getElementById(stand2.brick).innerHTML = stand2.img;
-    } else {
-         document.getElementById('bord').style.display = "none";
-    } 
 }
-   
+Game()
 
-
+function draw_text() { 
+    alert("wokred")
+    dice.style.display = "block";
+    btn.style.display = "none";
 }
-
+      
