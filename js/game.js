ff762242283bd143b2d9a29aebe6f46f15fa8a7e
unknown <danieleidenilsen@hotmail.com>
2020-02-26 20:42:38 +0100

    update

--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-
 class player { 
     constructor(name,brick,img) { 
         this.name = name;
@@ -14,16 +13,14 @@ var bord = document.getElementById('bordOne');
 var bord2 = document.getElementById('bordbrikstwo');
 var bord3 = document.getElementById('boardthree');
 var traps = [8,13,30,21,28,35];
-var stand =  new player("PlayerOne",1,"<img src='../img/Untitled-1.png'>");
-var stand2 = new player("Playertwo",1,"<img src='../playerIcon/player_icon_two.png'>");
+var stand =  new player("PlayerOne",1,"<img id ='one' src='../playerIcon/iconOne.png'>");
+var stand2 = new player("Playertwo",1,"<img id ='two' src='../playerIcon/player_icon_two.png'>");
 let task_one = document.getElementById('taskone');
 var dice = document.getElementById ('dice');
 var dice2 = document.getElementById('dice2');
-
 var fakedice = document.getElementById('fakedice');
+var playerOne = document.querySelector("#one"); 
 
-
-let PlayDice = true;
 
 for(var i = 13; i>0;i--)  { 
     bord3.innerHTML += "<div  id = "+[i]+" class = 'brick'></div>";
@@ -39,16 +36,23 @@ traps.forEach(element => {
 
 });
 
-
 function Game() {    
+   
+   
 
-    dice2.addEventListener('click',function() { 
-        document.getElementById(stand2.brick).innerHTML = "";          
-        let randNumb = Math.floor(Math.random()*6)+1;          
+    dice2.addEventListener('click',function() {  
+        if(stand.brick == stand2.brick) {
+            document.getElementById(stand.brick).innerHTML += stand.img;
+         } else {
+            var playerOne = document.querySelector("#two"); 
+            playerOne.parentNode.removeChild(playerOne);  
+         }
+         
+        let randNumb = Math.floor(Math.random()*6)+1;  
+      
         stand2.brick +=randNumb;   
         console.log(stand2.brick);
         moveplayer(stand2.brick,stand2.img);
-        console.log("Dic2")
       
         if(stand2.brick == 8 || stand2.brick == 13 || stand2.brick == 21 || stand2.brick == 28 || stand2.brick == 30 || stand2.brick == 35) {
             dice2.style.display = "none";
@@ -58,18 +62,23 @@ function Game() {
             dice2.style.display = "none";
             dice.style.display = "block";
         }       
-
     });
 
  
     dice.addEventListener('click',function(){ 
-        document.getElementById(stand.brick).innerHTML = "";          
-        var randNumb = Math.floor(Math.random()*6)+1;          
+        if(stand.brick == stand2.brick) {           
+            document.getElementById(stand2.brick).innerHTML += stand2.img;
+         } else {
+            var playerOne = document.querySelector("#one");
+            playerOne.parentNode.removeChild(playerOne);         
+ 
+        }
+           
+              
+        var randNumb = Math.floor(Math.random()*6)+1;  
         stand.brick +=randNumb;   
         console.log(stand.brick);
-        moveplayer(stand.brick,stand.img);
-     
-       
+        moveplayer(stand.brick,stand.img);              
         if(stand.brick == 8 || stand.brick == 13 || stand.brick == 21 || stand.brick == 28 ||  stand.brick == 30 || stand.brick == 35) { 
             dice.style.display = "none";
             dice2.style.display = "none";
@@ -78,12 +87,11 @@ function Game() {
             console.log("Dice1")
             dice.style.display = "none";
             dice2.style.display = "block";
-        }
-
-    
+        }    
     });
     
-    function moveplayer(arg1,arg2) {         
+    function moveplayer(arg1,arg2) {       
+  
 
         if(arg1 == 8) {            
             let text = "Did John snow killed Dragon Lady?";
@@ -102,19 +110,17 @@ function Game() {
             let wrong = "<button onclick='wrong("+arg1+");'>No</button>";          
             text_text(text,wrong,Qurrect);   
             dice.style.display = "none";
-            dice2.style.display = "none";       
-               
+            dice2.style.display = "none";     
         } 
-
         if(arg1 == 21)   {          
             let text = "How many seasons are there of Game of Thornes";
             let Qurrect = "<button onclick='correct();'>8</button>";
             let wrong = "<button onclick='wrong("+arg1+");'>No</button>";            
             text_text(text,Qurrect,wrong);
             dice.style.display = "none";
-            dice2.style.display = "none";      
-                   
-        }    
+            dice2.style.display = "none";     
+        } 
+
         if(arg1 == 28)   {           
             let text = "How many seasons are there of Game of Thornes";
             let Qurrect = "<button onclick='correct();'>8</button>";
@@ -123,10 +129,8 @@ function Game() {
             dice.style.display = "none";
             dice2.style.display = "none";            
         }    
-
-         
-        if(arg1 == 30)   {
-            
+        
+        if(arg1 == 30)  {            
             let text = "How many seasons are there of Game of Thornes";
             let Qurrect = "<button onclick='correct();'>8</button>";
             let wrong = "<button onclick='wrong("+arg1+");'>No</button>";             
@@ -134,8 +138,7 @@ function Game() {
             dice.style.display = "none";
             dice2.style.display = "none";             
         } 
-        if(arg1 == 35)   {
-            
+        if(arg1 == 35) {            
             let text = "How many seasons are there of Game of Thornes";
             let Qurrect = "<button onclick='correct();'>8</button>";
             let wrong = "<button onclick='wrong("+arg1+");'>No</button>";             
@@ -144,7 +147,8 @@ function Game() {
             dice2.style.display = "none";           
         }
         
-        if(arg1 < 40) { 
+        if(arg1 < 40) {
+            
             return document.getElementById(arg1).innerHTML = arg2;
         } 
         if(arg1 > 40) { 
@@ -154,14 +158,7 @@ function Game() {
 }
 Game();
 
-function draw_text() { 
-    alert("wokred")
-    dice.style.display = "block";
-    btn.style.display = "none";
-}
-
 function text_text(arg,arg2,arg3) {  
-
     textDisplay.innerHTML += arg ;
     textDisplay.innerHTML += arg2;
     textDisplay.innerHTML += arg3;
@@ -174,17 +171,11 @@ function correct() {
     if(stand.brick == 8 || stand.brick == 13 || stand.brick == 21 || stand.brick == 28 ||  stand.brick == 30 || stand.brick == 35) { 
         dice.style.display = "none";
         dice2.style.display = "block";
-
-
     }
     else if(stand2.brick == 8 || stand2.brick == 13 || stand2.brick == 21 || stand2.brick == 28 ||  stand2.brick == 30 || stand2.brick == 35) {
         dice2.style.display = "none";
         dice.style.display = "block";
-
-    }
-   
-    
-    
+    }   
 }
 
 function wrong(arg) {    
@@ -197,21 +188,16 @@ function wrong(arg) {
         dice2.style.display = "block";
         stand.brick = sum;
         console.log("test",stand.brick)
-       return document.getElementById(stand.brick).innerHTML = stand.img;
-       
+       return document.getElementById(stand.brick).innerHTML = stand.img;       
     }
-     if(stand2.brick == 8 || stand2.brick == 13 || stand2.brick == 21 || stand2.brick == 28 || stand2.brick == 25 || stand2.brick == 30 || stand2.brick == 35) {
+    if(stand2.brick == 8 || stand2.brick == 13 || stand2.brick == 21 || stand2.brick == 28 || stand2.brick == 25 || stand2.brick == 30 || stand2.brick == 35) {
          document.getElementById(stand2.brick).innerHTML = "";   
         dice2.style.display = "none";  
         dice.style.display = "block";
         stand2.brick = sum;
         console.log("test",stand2.brick)
         return document.getElementById(stand2.brick).innerHTML = stand2.img;
-
-    }
-       
+    }       
 }  
 
 
-
-      
